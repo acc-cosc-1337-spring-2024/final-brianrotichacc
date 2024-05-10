1ccd5e198adce22847a931b7e7c4d37c4a2c67ee
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-05-10 20:59:01 +0000

    question d submission

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,18 @@
-#add import
+import question_d as d
+
+def display_menu():
+    print(" Option 1: Display Multiplication Table\n Option 2: Exit ")
+
+display_menu()
+while True:
+    choose_value = int(input("Please enter your option: "))
+    if choose_value == 1:
+        table_list = d.create_multiplication_table()
+        d.display_multiplication_table(table_list)
+    elif choose_value == 2:
+        exit()
+    else:
+        print("Please enter a valid choice")
+    keep_going = input("Do you want to continue? (Y/N): ").lower()
+    if keep_going != 'y':
+        exit() 
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,14 @@
-#write functions here, don't add input('') statements here!
+def create_multiplication_table():
+    table = []
+    for i in range(1, 6):
+        row = []
+        for j in range(1, 11):
+            row.append(i * j)
+        table.append(row)
+    return table
+
+def display_multiplication_table(table):
+    for row in table:
+        for value in row:
+            print(f"{value:<3}", end=" ")
+        print()
