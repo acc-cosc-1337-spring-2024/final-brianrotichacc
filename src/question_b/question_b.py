4872e763d3efab5e77182155161fc07a09c0fc9f
brianrotichacc <157081499+brianrotichacc@users.noreply.github.com>
2024-05-10 17:46:28 +0000

    question b update

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,21 @@
-#write functions here, don't add input('') statements here!
+import stock as s
+def stock_purchase_history():
+    stock1 = s.Stock("AAPL", "Apple Inc")
+    stock2 = s.Stock("CAT", "Caterpillar")
+    stock3 = s.Stock("EK", "Eastman Kodak")
+    stock4 = s.Stock("GOOG", "Google")
+    stock5 = s.Stock("MSFT", "Microsoft")
+
+    stocks_dict = {
+    stock1.get_symbol(): stock1,
+    stock2.get_symbol(): stock2,
+    stock3.get_symbol(): stock3,
+    stock4.get_symbol(): stock4,
+    stock5.get_symbol(): stock5
+    }
+
+    print(f"{'Symbol':<10}{'Company Name'}")
+    print("---------------------------------")
+
+    for symbol, stock in stocks_dict.items():
+        print(f"{symbol:<10}{stock.get_company_name()}")
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,12 +1,25 @@
 #write function tests here, don't add input('') statements here!
+import io
 import unittest
-
+from unittest.mock import patch
+from src.question_a.question_a import numbers
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
 
 class Test_Config(unittest.TestCase):
+    @patch('builtins.input', side_effect=['1', '2', '3', '4', '5'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_question_a_numbers(self, mock_stdout, mock_input):
+        numbers()
 
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
+        output = mock_stdout.getvalue()
+
+        # Check the expected output
+        expected_output = "\nResults:\n"
+        expected_output += "The lowest number in the list: 1\n"
+        expected_output += "The highest number in the list: 5\n"
+        expected_output += "The total of the numbers in the list: 15\n"
+        expected_output += "The average of the numbers in the list: 3.0\n"
+        
+        self.assertIn(expected_output, output)
 
 
